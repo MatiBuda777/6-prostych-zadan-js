bf5f62f5b3ad71fd50badcc66f1952ff380892cd
Mateusz <mateusz.budek@cyfroweszkoly.pl>
2024-11-29 08:33:01 +0100

    Finish work on simple-text-processor.js

--- zad1/simple-text-processor.js
+++ zad1/simple-text-processor.js
@@ -1,13 +1,22 @@
 const text = "abcDEfGhi-jk_l_+mNOp.."
 const processText = (text, { uppercase = false, reverse = false, removeSpecialChar = false } = {}) => {
-    const { ...rest } = {uppercase, reverse, removeSpecialChar};
-    let textReverse = ""
-    let textRemoveSpecialChar = ""
+    
     if (uppercase == true){
         let textUppercase = text.toUpperCase()
         console.log(textUppercase)
     }
-    
+    if (reverse == true){
+        const [ ...rest ] = text;
+        rest.reverse()
+        let textReverse = rest.join("")
+        console.log(textReverse)
+    }
+    if (removeSpecialChar == true){
+        let textRemoveSpecialChar = text.replace(/[^a-zA-Z ]/g, '') // regex (regular expression)
+        console.log(textRemoveSpecialChar)
+    }
 }
+
+processText(text, {uppercase: true, reverse: true, removeSpecialChar: true})
 
 
