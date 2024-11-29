4c006df0387061fffb1e5bc684d8bc6c512913a1
Mateusz <mateusz.budek@cyfroweszkoly.pl>
2024-11-29 08:57:33 +0100

    change math-calc.js, finish zad1 and zad2

--- zad2/math-calc.js
+++ zad2/math-calc.js
@@ -1,26 +1,26 @@
 class Calculator {
     static add(...rest){
         let sum = rest.reduce((acc, num) => acc + num)
-        return sum
+        console.log(`Suma liczb ${rest} to ${sum}`)
     }
 
     static subtract(...rest){
         let difference = rest.reduce((acc, num) => acc - num)
-        return difference
+        console.log(`Różnica liczb ${rest} to ${difference}`)
     }
 
     static multiply(...rest){
         let product = rest.reduce((acc, num) => acc * num)
-        return product
+        console.log(`Iloczyn liczb ${rest} to ${product}`)
     }
 
     static divide(...rest){
         let ratio = rest.reduce((acc, num) => acc / num)
-        return ratio
+        console.log(`Iloraz liczb ${rest} to ${ratio}`)
     }
 }
 
-console.log(Calculator.add(1,2,3,4,5))
-console.log(Calculator.subtract(10,2,3))
-console.log(Calculator.multiply(2,5,10,20))
-console.log(Calculator.divide(1000,10,2))
+Calculator.add(1,2,3,4,5)
+Calculator.subtract(10,2,3)
+Calculator.multiply(2,5,10,20)
+Calculator.divide(1000,10,2)
